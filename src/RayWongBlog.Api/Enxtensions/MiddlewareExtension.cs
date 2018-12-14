f7cc06059cfec4c2d89c562f0bb6ccfa96a93b76
wanghaoxing <wang.haoxing@synyi.com>
2018-12-14 17:52:13 +0800

    init

---
+++ src/RayWongBlog.Api/Enxtensions/MiddlewareExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RayWongBlog.Api.Enxtensions
+{
+    public static class MiddlewareExtension
+    {
+
+        public static IServiceCollection AddMiddleware(this IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            return services;
+        }
+    }
+}
---
+++ src/RayWongBlog.Api/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RayWongBlog.Api.Enxtensions;
+
+namespace RayWongBlog.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMiddleware();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
