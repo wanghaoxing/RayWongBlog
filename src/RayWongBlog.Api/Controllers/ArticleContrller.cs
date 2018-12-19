d2a9c108fc264ccf423355776984cca252f756c4
wanghaoxing <1035842070@qq.com>
2018-12-19 23:04:34 +0800

    1,实现HATEOAS,RESTful API最好做到Hypermedia，即返回结果中提供链接，连向其他API方法，使得用户不查文档，也知道下一步应该做什么 2,增加自定义媒体类型原因是因为根据restful设计约束,统一接口里的自我描述的相应,比如我请求的是articles资源,mediatype是application/json的话,应该是这个对象的json,实际上我们做到hateoas以后就不是这个对象的application/json了,所以自定义媒体类型 3.自定义媒体类型格式要求application/vnd.yourformat+json

--- src/RayWongBlog.Api/Controllers/ArticleContrller.cs
+++ src/RayWongBlog.Api/Controllers/ArticleContrller.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using RayWongBlog.Api.Helpers;
 using RayWongBlog.Domain.Interfaces.Repositorys;
 using RayWongBlog.Domain.Models.Entitys;
 using RayWongBlog.Domain.Models.ViewModels;
@@ -40,8 +41,9 @@ namespace RayWongBlog.Api.Controllers
             _mapper = mapper;
             _urlHelper = urlHelper;
         }
-        [HttpGet(Name ="GetArticles")]
-        public async Task<IActionResult> Get([FromQuery]ArticleParameters request)
+        [HttpGet(Name = "GetArticles")]
+        [RequestHeaderMatchingMediaType("Accept", new[] { "application/vnd.raywongblog.hateoas+json" })]
+        public async Task<IActionResult> Get([FromQuery]ArticleParameters request,[FromHeader(Name ="Accept")] string mediaType)
         {
             var list = await _articleRepository.GetAllArticlesAsync(request);
             var previoustlink = list.GetHasPrevious ? CreateUri(request, PaginationUriType.PreviousPage) : null;
@@ -51,8 +53,8 @@ namespace RayWongBlog.Api.Controllers
                 PageSize = list.PageSize,
                 PageIndex = list.PageIndex,
                 TotalCount = list.TotalCount,
-                PageCount = list.PageCount,           
-                Previoustlink=previoustlink,
+                PageCount = list.PageCount,
+                Previoustlink = previoustlink,
                 Nextlink = nextlink,
                 //list.GetHasPrevious
             };
@@ -64,12 +66,24 @@ namespace RayWongBlog.Api.Controllers
                 ContractResolver = new CamelCasePropertyNamesContractResolver()
             }));//序列化时候变成驼峰规范
             var viewList = _mapper.Map<IEnumerable<Article>, IEnumerable<ArticleViewModel>>(list);
-            var result = viewList.ToDynamicIEnumerable(request.Fields);
-            return Ok(result);
+            var shaped = viewList.ToDynamicIEnumerable(request.Fields);
+            var shapedLinks = shaped.Select(r =>
+              {
+                  var dic = r as IDictionary<string, object>;
+                  var articlelinks = CreateLinks((int)dic["Id"], request.Fields);
+                  dic.Add("links", articlelinks);
+                  return dic;
+              });
+            var links = CreateLinks(request, list.GetHasPrevious, list.GetHasNext);
+            return Ok(new
+            {
+                value=shapedLinks,
+                links
+            });
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
+        [HttpGet("{id}", Name = "GetArticle")]
+        public async Task<IActionResult> Get(int id, string fields)
         {
             var article = await _articleRepository.GetArticleByIdAsync(id);
             if (article == null)
@@ -77,7 +91,11 @@ namespace RayWongBlog.Api.Controllers
                 return NotFound();
             }
             var articleViewModel = _mapper.Map<Article, ArticleViewModel>(article);
-            return Ok(articleViewModel);
+            var shaped = articleViewModel.ToDynamic(fields);
+            var links = CreateLinks(id, fields);
+            var result=shaped as IDictionary<string,object>;
+            result.Add("links", links);
+            return Ok(result);
         }
         [HttpPost]
         public async Task<IActionResult> Post()
@@ -127,6 +145,55 @@ namespace RayWongBlog.Api.Controllers
                     };
                     return _urlHelper.Link("GetArticles", current);
             }
+        }
+
+        private IEnumerable<LinkResource> CreateLinks(int id, string fields = null)
+        {
+            var links = new List<LinkResource>();
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                links.Add(new LinkResource(
+                    _urlHelper.Link("GetArticle", new { id }), "self", "GET"));
+
+            }
+            else
+            {
+                links.Add(new LinkResource(
+                                    _urlHelper.Link("GetArticle", new { id, fields }), "self", "GET"));
+            }
+            links.Add(
+                new LinkResource(
+                    _urlHelper.Link("DeleteArticle", new { id }), "delete_article", "DELETE"));
+            return links;
+
+        }
+        private IEnumerable<LinkResource> CreateLinks(ArticleParameters request,
+     bool hasPrevious, bool hasNext)
+        {
+            var links = new List<LinkResource>
+            {
+                new LinkResource(
+                    CreateUri(request, PaginationUriType.CurrentPage),
+                    "self", "GET")
+            };
+
+            if (hasPrevious)
+            {
+                links.Add(
+                    new LinkResource(
+                        CreateUri(request, PaginationUriType.PreviousPage),
+                        "previous_page", "GET"));
+            }
+
+            if (hasNext)
+            {
+                links.Add(
+                    new LinkResource(
+                        CreateUri(request, PaginationUriType.NextPage),
+                        "next_page", "GET"));
+            }
+
+            return links;
         }
     }
 }
--- src/RayWongBlog.Api/Enxtensions/MiddlewareExtension.cs
+++ src/RayWongBlog.Api/Enxtensions/MiddlewareExtension.cs
@@ -22,9 +22,15 @@ namespace RayWongBlog.Api.Enxtensions
                     options.ReturnHttpNotAcceptable = true;//启用内容协商,效果是请求头里添加的需要的返回类型,如果服务端不支持,就会返回406,例如API消费者请求的是application/xml格式的media type，而API只支持application/json
 
                     options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());//比如客户端请求Accept,application/xml,增加输出格式的支持
+                    var outFormats = options.OutputFormatters.OfType<JsonOutputFormatter>().FirstOrDefault();
+                    if (outFormats != null)
+                    {
+                        outFormats.SupportedMediaTypes.Add("application/vnd.raywongblog.hateoas+json");
+
+                    }
                 })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-                .AddJsonOptions(options=>
+                .AddJsonOptions(options =>
                 {
                     options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
                 });
